99d5c953b197ebc319d71fe098b5907f989466d9
João Mesquita <151409184+bacon720kg@users.noreply.github.com>
2023-12-12 00:24:47 +0000

    Update AOC_2_3.asm

--- Trabalho/AOC_2_3.asm
+++ Trabalho/AOC_2_3.asm
@@ -1,15 +1,23 @@
 # Escreva um programa que leia 3 números inteiros do utilizador, garantindo que todos eles são positivos e diferentes entre si. Se o utilizador tentar introduzir um valor igual a um valor já introduzido, o programa deverá repetir o pedido de introdução do valor.
-# EM CONSTRUÇÃO
+# EM CONSTRUÇÃO: Como validar se o número introduzido é inteiro sem o programa rebentar?
 
 .data
 	barra: .asciiz "\n****************************************************************"
 	intro: .asciiz "\n Introduza 3 números inteiros, positivos e diferentes entre si."
-	entrada: .asciiz "\n Introduza um dos números: "
-	
+	str1: .asciiz "\n Introduza o primeiro número inteiro e positivo: "
+	str2: .asciiz "\n Introduza o segundo número inteiro, positivo e diferente do anterior: "
+	str3: .asciiz "\n Introduza o último número inteiro, positivo e diferente dos anteriores: "
+	erro: .asciiz " Erro: número introduzido inválido. Tente novamente."
+	fim: .asciiz "\n Os três números introduzidos foram "
+	vir: .asciiz ", "
+	e: .asciiz " e "
+	ponto: .asciiz "."
+	espaço: .asciiz "\n "
 
 .text
 .globl main
 main:
+	# Introdução
 	li $v0, 4
 	la $a0, barra
 	syscall
@@ -19,20 +27,101 @@ main:
 	li $v0, 4
 	la $a0, barra
 	syscall
+	li $v0, 4
+	la $a0, espaço
+	syscall
 	
-	li $t0, 1
-	
-	loop:
+	# Leitura do primeiro número
+	primeiro:
 		li $v0, 4
-		la $a0, entrada
+		la $a0, str1
 		syscall
-
+		li $v0, 5
+		syscall
+		move $t0, $v0
+	
+		blez $t0, erro1
+		b segundo
+	
+		erro1:
+			li $v0, 4
+			la $a0, erro
+			syscall
+			b primeiro
+	
+	# Leitura do segundo número
+	segundo:
+		li $v0, 4
+		la $a0, str2
+		syscall
 		li $v0, 5
 		syscall
 		move $t1, $v0
-		
-		add $t0, $t0, 1
-		ble $t0, 3, loop
 	
-	li $v0, 10
-	syscall
+		bgtz $t1, diferente
+		b erro2
+		
+		diferente:
+			beq $t0, $t1, erro2
+			b terceiro
+			
+		erro2:
+			li $v0, 4
+			la $a0, erro
+			syscall
+			b segundo
+	
+	# Leitura do terceiro número
+	terceiro:
+		li $v0, 4
+		la $a0, str3
+		syscall
+		li $v0, 5
+		syscall
+		move $t2, $v0
+		
+		bgtz $t2, diferente2
+		b erro3
+		
+		diferente2:
+			beq $t0, $t2, erro3
+			beq $t1, $t2, erro3
+			b imprimir
+			
+		erro3:
+			li $v0, 4
+			la $a0, erro
+			syscall
+			b terceiro
+	
+	# Impressão dos números introduzidos
+	imprimir:
+		li $v0, 4
+		la $a0, barra
+		syscall
+		li $v0, 4
+		la $a0, fim
+		syscall
+		move $a0, $t0
+		li $v0, 1
+		syscall
+		li $v0, 4
+		la $a0, vir
+		syscall
+		move $a0, $t1
+		li $v0, 1
+		syscall
+		li $v0, 4
+		la $a0, e
+		syscall
+		move $a0, $t2
+		li $v0, 1
+		syscall
+		li $v0, 4
+		la $a0, ponto
+		syscall
+		li $v0, 4
+		la $a0, barra
+		syscall
+		li $v0, 10
+		syscall
